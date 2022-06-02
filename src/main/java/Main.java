e7d388dab373b25273ce2e4326449076a9779a82
Emilia <uliakas2112@gmail.com>
2022-06-02 12:18:40 +0200

    add dependencies (now really); add model class for exchange rates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
---
+++ src/main/java/model/CurrencyRates.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.Map;
+
+/**
+ * Sample:
+ *
+ * {
+ *   "disclaimer": "Usage subject to terms: https://openexchangerates.org/terms", <- ignored
+ *   "license": "https://openexchangerates.org/license", <- ignored
+ *   "timestamp": 1608595199,
+ *   "base": "USD",
+ *   "rates": {
+ *     "AED": 3.6731,
+ *     "AFN": 77.149997,
+ *      // more
+ *   }
+ * }
+ */
+public class CurrencyRates {
+
+    private Integer timestamp;
+    private String base;
+    private Map<String, Double> rates;
+
+    public CurrencyRates() {
+    }
+
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+}
